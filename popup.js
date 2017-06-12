ae7a0ca671096e423342297efd6ad8fd42c73895
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-11 23:03:24 -0400

    Determine url of current tab

--- popup.js
+++ popup.js
@@ -1,8 +1,28 @@
-function renderText(text) {
-  document.getElementById('witLI').textContent = text;
+/* global chrome */
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  }
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0]
+    var url = tab.url
+    console.assert(typeof url == 'string', 'tab.url should be a string')
+    callback(url)
+  })
+}
+
+function renderHTML(value) {
+  document.getElementById('witLI').innerHTML = value;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  renderText('Test #2')
+  getCurrentTabUrl(function(url) {
+    if(url === 'https://www.linkedin.com/mynetwork/'){
+      renderHTML('The current URL is: ' + url)
+    }else{
+      renderHTML('Please navigate to https://www.linkedin.com/mynetwork/')
+    }
+  })
 })
-
