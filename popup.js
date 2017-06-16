67ddaed5557e83b2051e9b2627de4fa4986ea9f1
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-16 14:13:35 -0400

    Can analyze single static image

--- popup.js
+++ popup.js
@@ -1,14 +1,4 @@
-/* global chrome clarApp */
-
-// clarApp.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAvzAAAAJDAxZGE0OWM4LTk3M2UtNDE5Ny1iM2YxLTkxOGJmMDFjZGQwZg.jpg')
-//   .then(
-//     function(response) {
-//       console.log('Response', response)
-//     },
-//     function(err) {
-//       console.log('Error', err)
-//     }
-//   )
+/* global chrome */
 
 function getCurrentTabUrl(callback) {
   var queryInfo = {
@@ -25,13 +15,21 @@ function getCurrentTabUrl(callback) {
 
 function consoleLog() {
   chrome.tabs.executeScript(null,
-      {code: `
-        var clarApp = new Clarifai.App(
-          'zYOQwhlm1J3ylwqW4AZyArXbDvmxMfNh521S4iYH',
-          'ya_pWvenG-kZ5V5VhMfpmu1CNRiK5xVmiAUAPXxn'
+    {code: `
+      var clarApp = new Clarifai.App(
+        'zYOQwhlm1J3ylwqW4AZyArXbDvmxMfNh521S4iYH',
+        'ya_pWvenG-kZ5V5VhMfpmu1CNRiK5xVmiAUAPXxn'
+      )
+      clarApp.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAvzAAAAJDAxZGE0OWM4LTk3M2UtNDE5Ny1iM2YxLTkxOGJmMDFjZGQwZg.jpg')
+        .then(
+          function(response) {
+            console.log('Response', response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].name, response.outputs[0].data.regions[0].data.face.gender_appearance.concepts[1].value)
+          },
+          function(err) {
+            console.log('Error', err)
+          }
         )
-        console.log(clarApp)
-      `})
+    `})
   setTimeout(function(){
     window.close()
   }, 2000)
@@ -43,23 +41,22 @@ function renderHTML(value) {
 
 function onScroll() {
   chrome.tabs.executeScript(null,
-      {code: `
-        window.addEventListener('scroll', function(e){
-          console.log('test', document.documentElement.scrollHeight)
-        })
-      `})
-  window.close()
+    {code: `
+      window.addEventListener('scroll', function(e){
+        console.log('test', document.documentElement.scrollHeight)
+      })
+    `})
 }
 
 function cleanFeed() {
   chrome.tabs.executeScript(null,
-      {code: `
-        window.addEventListener('scroll', function(e){
-          [].forEach.call(document.querySelectorAll('.feed-s-connection-updates'),function(e){
-            e.parentNode.removeChild(e);
-          })
+    {code: `
+      window.addEventListener('scroll', function(e){
+        [].forEach.call(document.querySelectorAll('.feed-s-connection-updates'),function(e){
+          e.parentNode.removeChild(e);
         })
-      `})
+      })
+    `})
   setTimeout(function(){
     window.close()
   }, 2000)
@@ -68,14 +65,14 @@ function cleanFeed() {
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     if(url === 'https://www.linkedin.com/mynetwork/'){
-      renderHTML('Filtering...')
+      renderHTML('Analyzing...')
+      consoleLog()
       onScroll()
     }else if(url === 'https://www.linkedin.com/feed/'){
       renderHTML('Feed will be cleaned as you scroll.')
       cleanFeed()
     }else{
       renderHTML('Please navigate to https://www.linkedin.com/mynetwork/')
-      consoleLog()
     }
   })
 })
