d5d6e447e1641764b45f5c2f125d9f4ac160b07a
Christine <cleverett1@gmail.com>
2017-06-17 17:21:58 -0400

    d3 successfully added

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// chrome.browserAction.onClicked.addListener(function(tab){
-//   chrome.tabs.executeScript( {file: "d3.min.js" });
-//   chrome.tabs.executeScript( {file: "data.js" })
-// });
+chrome.browserAction.onClicked.addListener(function(tab){
+  chrome.tabs.executeScript( {file: "d3.js" });
+  chrome.tabs.executeScript( {file: "data.js" })
+});
