9199732906929e194994011558f163ed06905697
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-25 13:05:48 -0400

    code cleanup

--- content.js
+++ content.js
@@ -1,50 +1,50 @@
 /* global Clarifai clarKey clarSecret */
 
-var clarApp = new Clarifai.App(clarKey, clarSecret)
+var clarApp = new Clarifai.App(clarKey, clarSecret);
 
 function femalePercent(nodeArr) {
-  var i = 0
-  var len = nodeArr.length - 1
+  var i = 0;
+  var len = nodeArr.length - 1;
   function filter() {
-    let node = nodeArr[i]
-    i++
+    let node = nodeArr[i];
+    i++;
     if (node) {
       clarApp.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', node.src)
         .then(
         function (response) {
           if (response) {
-            let resUrl = response.outputs[0].input.data.image.url
-            let nodeUrl = node.src
+            let resUrl = response.outputs[0].input.data.image.url;
+            let nodeUrl = node.src;
             if (resUrl === nodeUrl) {
-              var genders = response.outputs[0].data.regions[0].data.face.gender_appearance.concepts
-              let percentFemale = 0
+              var genders = response.outputs[0].data.regions[0].data.face.gender_appearance.concepts;
+              let percentFemale = 0;
               if (genders[0].name === 'feminine') {
-                percentFemale = genders[0].value
+                percentFemale = genders[0].value;
               } else {
-                percentFemale = genders[1].value
+                percentFemale = genders[1].value;
               }
               if (percentFemale < 0.5) {
-                node.closest('li.mn-pymk-list__card').remove()
-                console.log(node.alt, 'MALE')
+                node.closest('li.mn-pymk-list__card').remove();
+                console.log(node.alt, 'MALE');
               }
               else {
-                console.log(node.alt, 'FEMALE')
+                console.log(node.alt, 'FEMALE');
               }
             } else {
-              console.log('******SYNC_ISSUE********')
-              i--
+              console.log('******SYNC_ISSUE********');
+              i--;
             }
           }
         },
         function (err) {
-          console.log(err)
-          console.log('Error on', node.alt)
+          console.log(err);
+          console.log('Error on', node.alt);
         }
-        )
+        );
       if (i < len) {
-        setTimeout(filter, 600)
+        setTimeout(filter, 600);
       }
     }
   }
-  filter()
+  filter();
 }
